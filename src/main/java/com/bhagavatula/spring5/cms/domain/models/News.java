0bb3cfa246803cfb2c38ad5e909aa1f742f565bc
DIGIKEYUS\venkata_bhagavatula <venkata.bhagavatula@digikey.com>
2018-06-04 17:19:35 -0500

    more code

--- src/main/java/com/bhagavatula/spring5/cms/domain/models/News.java
+++ src/main/java/com/bhagavatula/spring5/cms/domain/models/News.java
@@ -15,4 +15,15 @@ public class News {
     Set<Review> reviewers;
     Set<Category> categories;
     Set<Tag> tags;
+
+    public Review review(String userId, String status){
+        final Review review = new Review(userId, status);
+        this.reviewers.add(review);
+        return review;
+    }
+
+    public Boolean revised() {
+        return this.mandatoryReviewers.stream().allMatch(reviewer -> this.reviewers.stream()
+                .anyMatch(review -> reviewer.id.equals(review.userId) && "approved".equals(review.status)));
+    }
 }
